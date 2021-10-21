d143a49a20f1f4b19a7b56f4c2be37301d0fdc77
arpit <arpitshukla62@gmail.com>
2021-10-21 08:05:50 +0530

    Display notes sorted acc to their last updated time

--- app/src/main/java/com/arpit/notes/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/arpit/notes/ui/home/HomeViewModel.kt
@@ -3,11 +3,16 @@ package com.arpit.notes.ui.home
 import androidx.lifecycle.ViewModel
 import com.arpit.notes.data.NotesDao
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     notesDao: NotesDao
 ) : ViewModel() {
+
     val notes = notesDao.observeAllNotes()
+        .map { notes ->
+            notes.sortedByDescending { it.lastUpdatedAt }
+        }
 }
